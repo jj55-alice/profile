c1d9d54efd3d3fa69fe50aa28f32a641c0591035
ara <ara@araui-MacBookAir.local>
2025-05-01 21:01:58 +0900

    change home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
 import TimesTable from './pages/times-table'
 
 const App = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/times-table" element={<TimesTable />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<TimesTable />} />
         </Routes>
     </Router>
   )
