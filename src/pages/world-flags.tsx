44663f26ad610c10f710752e355fc2ca24d825af
ara <ara@araui-MacBookAir.local>
2025-05-04 16:28:08 +0900

    add world-flags

--- src/pages/world-flags.tsx
+++ src/pages/world-flags.tsx
@@ -1,7 +1,74 @@
+import { useMemo, useState } from 'react';
+import { COUNTRIES } from '../constants/countries';
+import { Button } from '@heroui/react';
+import { motion } from 'framer-motion';
+
+type CountryCode = keyof typeof COUNTRIES;
+
 const WorldFlags = () => {
+  const keyList = useMemo(() => Object.keys(COUNTRIES) as CountryCode[], []);
+  const [mode, setMode] = useState<'question' | 'answer' | 'none'>('none');
+  const [key, setKey] = useState<CountryCode>(keyList[0]);
+
+  const changeKey = () => {
+    setMode('question');
+
+    const newKey = Math.floor(Math.random() * keyList.length);
+    setKey(keyList[newKey]);
+  };
+
+  const handleStart = () => {
+    changeKey();
+  };
+
+  const handleClick = () => {
+    if (mode === 'none') {
+      return;
+    }
+
+    if (mode === 'question') {
+      setMode('answer');
+      const utterance = new SpeechSynthesisUtterance(`${COUNTRIES[key].name} ${COUNTRIES[key].capital}`);
+      speechSynthesis.speak(utterance);
+    }
+
+    if (mode === 'answer') {
+      changeKey();
+    }
+  };
   return (
-    <div>
-      <h1>TBD</h1>
+    <div onClick={handleClick} className="flex flex-col items-center gap-10">
+      <div className="flex h-[600px] w-full flex-row items-center justify-center gap-10 py-10">
+        {mode === 'none' ? (
+          <img
+            src="/world.png"
+            alt="world"
+          />
+        ) : (
+          <motion.img
+            key={key}
+            src={COUNTRIES[key].flag}
+            width="640"
+            alt={key}
+            className="border border-solid border-4 border-white"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          />
+        )}
+      </div>
+      {mode === 'none' && (
+        <Button size="lg" color="primary" onPress={handleStart}>
+          START
+        </Button>
+      )}
+      {mode === 'answer' && (
+        <div className="flex flex-col items-center">
+          <p className="text-[100px] font-bold text-[#fff]">{COUNTRIES[key].name}</p>
+          <p className="text-[60px] text-[#fff]">{COUNTRIES[key].capital}</p>
+        </div>
+      )}
     </div>
   );
 };
